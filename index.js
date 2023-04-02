42a6275d6b40c8c6915d60fa422e41a78d55fe6f
kainy01 <kainykawai@gmail.com>
2023-04-02 23:09:29 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  return Math.abs(42 - someValue);
+}
+
+function distanceFromHqInFeet(someValue) {
+  return 264 * distanceFromHqInBlocks(someValue);
+}
+
+function distanceTravelledInFeet(x, y) {
+  return Math.abs(x - y) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  let des = distanceTravelledInFeet(start, destination);
+  switch (true) {
+    case des <= 400:
+      return 0;
+    case des < 2000:
+      return (des - 400) * 0.02;
+    case des < 2500:
+      return 25;
+    case des > 2500:
+      return "cannot travel that far";
+  }
+}
